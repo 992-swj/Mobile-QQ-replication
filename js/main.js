f4c8e963ce18c81675c2e210d5120ac9f84bdbce
卑微 <2532605573@qq.com>
2020-04-10 20:57:25 +0800

    First upload - mobile QQ

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+   var placeholder= $(".input input").attr("placeholder");
+   var str="<span class='iconfont icon-chaxun'></span>";
+    str=placeholder+str;
+    
+
+});
